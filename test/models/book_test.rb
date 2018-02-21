9529f581b3cb0c2f9b2fc6a34f1f2a130e195315
Jordan Mynes <mynesjordan@gmail.com>
2018-02-21 17:18:30 -0600

    Edited tests...for testing true/false universally

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "not the truth" do
+    assert_not false
+  end
 end
